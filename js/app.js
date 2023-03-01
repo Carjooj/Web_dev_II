7a3bb061b15b6c566496a11a7e5ca1cc01b7613f
Carjooj <victor@4info.com.br>
2023-03-01 11:25:13 -0300

    framework express, manipular rota, nodemon, variaveis get no url

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
-var tabuada = require('./tabuada.js')
+const express = require("express")
+const app = express()
 
-tabuada(2, 1000)
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "html/index.html")
+})
+
+app.get("/produto/:item/:quantidade", function(req, res) {
+    res.send("Item " + req.params.item + " <br>Quantidade " + req.params.quantidade)
+})
+
+
+app.listen(8081, function() {
+    console.log("rodou")
+})
+
