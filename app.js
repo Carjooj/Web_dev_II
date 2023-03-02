c6b132e6827556ddeeec644ae1d292d3e19568c8
Carjooj <victor@4info.com.br>
2023-03-01 22:14:56 -0300

    tentando rodar a pagina no nodemon

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express")
+const path = require("path")
 const app = express()
 
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "html/index.html")
+    res.sendFile(__dirname + "/html/index.html")
+    //path.join
 })
 
 app.get("/produto/:item/:quantidade", function(req, res) {
@@ -13,13 +15,3 @@ app.get("/produto/:item/:quantidade", function(req, res) {
 app.listen(8081, function() {
     console.log("rodou")
 })
-
-
-var http = require('http')
-const soma = require('./soma.js')
-
-http.createServer (function(req, res){
-    res.end()
-}).listen(8081)
-
-console.log("on")
